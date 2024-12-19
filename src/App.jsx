0bdac88c636b3ee4ad8c063a7a64295eefc59396
lakinaik09 <lucky.digitaldeta@gmail.com>
2024-12-20 00:50:29 +0530

    route update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/fabrication' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
